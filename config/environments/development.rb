c2d1500d78c2f3f85098955264e0276a377fc26b
Samuel Stewart <samuelcstewart@icloud.com>
2015-08-06 11:09:33 +1200

    Try precompiling static assets for production mode to use in EB on development mode

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,6 +26,7 @@ Rails.application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
   config.serve_static_files = false
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
