3294e1f7e82b0f98a0561d6eed96eab2e0379b17
Samuel Stewart <samuelcstewart@icloud.com>
2015-08-06 12:47:13 +1200

    More development settings. trying to achieve same behaviour using loacl dev environment and dev rnvironment on EBS

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,4 @@
 Rails.application.configure do
-  # Settings specified here will take precedence over those in config/application.rb.
-
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
@@ -37,8 +35,8 @@ Rails.application.configure do
   config.assets.raise_runtime_errors = true
 
   # Disable serving static files from /public by default
-  config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
-  config.assets.compile = false
+  #config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.assets.compile = true
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
