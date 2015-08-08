62a18740b97c4d4ec1131569aa31ee9a45bf8f24
Samuel Stewart <samuelcstewart@icloud.com>
2015-08-08 14:23:18 +1200

    Change default timezone. Add Reports and Animal admin pages. Change bheaviour of location admin page so it only displays a single dog status for each location for now. (There can be several)

--- app/admin/locations.rb
+++ app/admin/locations.rb
@@ -1,10 +1,11 @@
 ActiveAdmin.register Location do
   filter :name
-  filter :dog_statuses
+  filter :dog_statuses_status, label: 'Dog Status', as: :select, collection: DogStatus.statuses
 
   permit_params :category, :name, :animal_blurb, :image, :latitude, :longitude,
     dog_statuses_attributes: [:location_id, :status, :guidelines]
 
+  # Converts lat/long form fields to geographic object before commiting object to database.
   before_create do |l|
     l.geolocation = RGeo::Geographic.spherical_factory(:srid => 4326).point(l.longitude, l.latitude)
   end
@@ -24,46 +25,31 @@ ActiveAdmin.register Location do
     end
   end
 
+  # Form. Conditionally add geolocation values to lat/long fields. Required to manipulate geolocations standard
+  # WKT output into something more editable.
   form do |f|
-    # Display different form if we're creating a new record. Violate DRY for readability and usability.
-    # This is necessary to fox activeadmin into exhibiting the behaviour I want it to exhibit for this
-    # model
-    if f.object.new_record?
-      f.inputs "Location Details" do
-        f.input :name
-        f.input :category, as: :select, collection: Location.categories.keys
-        f.input :animal_blurb
+    f.inputs "Location Details" do
+      f.input :name
+      f.input :category, as: :select, collection: Location.categories.keys
+      f.input :animal_blurb
+      if f.object.new_record?
         f.input :latitude, :as => :number
         f.input :longitude, :as => :number
-        f.input :image, :as => :file, :hint => f.object.image.present? \
-          ? image_tag(f.object.image.url(:medium))
-          : content_tag(:span, "no image uploaded yet")
-      end
-
-      # Create a new dog status object by default. Test if empty to avoid creating a new one if form submit fails.
-      f.object.dog_statuses.new if f.object.dog_statuses.empty?
-      f.inputs "Dog Information", for: [:dog_statuses, f.object.dog_statuses.last] do |ds|
-        ds.input :status, as: :select, collection: DogStatus.statuses.keys
-        ds.input :guidelines, as: :text
-      end
-
-    # Create form for editing
-    else
-      f.inputs "Location Details" do
-        f.input :name
-        f.input :category, as: :select, collection: Location.categories.keys
-        f.input :animal_blurb
+      else
         f.input :latitude, :as => :number, :input_html => { :value => f.object.geolocation.y }
         f.input :longitude, :as => :number, :input_html => { :value => f.object.geolocation.x }
-        f.input :image, :as => :file, :hint => f.object.image.present? \
-          ? image_tag(f.object.image.url(:medium))
-          : content_tag(:span, "no image uploaded yet")
       end
-      f.has_many :dog_statuses do |ds|
-        ds.input :status, as: :select, collection: DogStatus.statuses.keys
-        ds.input :guidelines, as: :text
-      end
+      f.input :image, :as => :file, :hint => f.object.image.present? \
+        ? image_tag(f.object.image.url(:medium))
+        : content_tag(:span, "no image uploaded yet")
+    end
 
+    # Create a new dog status object by default. Gets around the default active admin behaviour of adding
+    # buttons to create a new status.
+    f.object.dog_statuses.new if f.object.dog_statuses.empty?
+    f.inputs "Dog Information", for: [:dog_statuses, f.object.dog_statuses.last] do |ds|
+      ds.input :status, as: :select, collection: DogStatus.statuses.keys
+      ds.input :guidelines, as: :text
     end
     f.actions
   end
