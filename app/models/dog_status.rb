d40f3092c2a314bda2f7f6e7fbb37a1019bd9bbc
Samuel Stewart <samuelcstewart@icloud.com>
2015-08-13 22:19:08 +1200

    Add after_save validation to update parent location when a new dog status is created

--- app/models/dog_status.rb
+++ app/models/dog_status.rb
@@ -1,4 +1,7 @@
 class DogStatus < ActiveRecord::Base
+  after_save do
+    location.update_attribute(:updated_at, Time.now)
+  end
   belongs_to :location
   validates :status, :presence => true
   validates :guidelines, :presence => true
