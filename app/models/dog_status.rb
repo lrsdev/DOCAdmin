7898cda9d3e88be2b0252d2f00bc473d65a79e62
Samuel Stewart <samuelcstewart@icloud.com>
2015-08-30 23:46:11 +1200

    Add before_destroy hook to ensure dog status cannot be deleted if it is locations only child

--- app/models/dog_status.rb
+++ app/models/dog_status.rb
@@ -2,8 +2,18 @@ class DogStatus < ActiveRecord::Base
   after_save do
     location.update_attribute(:updated_at, Time.now)
   end
+
+  before_destroy do
+    location_has_status?
+  end
+
   belongs_to :location
   validates :status, :presence => true
   validates :guidelines, :presence => true
   enum status: [:off_lead, :on_lead, :no_dogs]
+
+  def location_has_status?
+    errors.add(:base, 'Location must have one dog status') if location.dog_statuses.count == 1
+    return false
+  end
 end
