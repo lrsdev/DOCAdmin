0d4eb8a55975288036667bab8b4555646e18b01d
Samuel Stewart <samuelcstewart@icloud.com>
2015-08-31 01:11:27 +1200

    Revert dog status nested form behaviour to standard

--- app/admin/locations.rb
+++ app/admin/locations.rb
@@ -4,7 +4,7 @@ ActiveAdmin.register Location do
   #filter :current_dog_status_status, label: 'Dog Status', as: :select, collection: DogStatus.statuses
 
   permit_params :category, :name, :animal_blurb, :image, :latitude, :longitude, :active,
-    dog_statuses_attributes: [:location_id, :status, :guidelines]
+    dog_statuses_attributes: [:location_id, :status, :guidelines, :id, :_destroy]
 
   # Converts lat/long form fields to geographic object before commiting object to database.
   before_create do |l|
@@ -13,20 +13,6 @@ ActiveAdmin.register Location do
 
   before_update do |l|
     l.geolocation = RGeo::Geographic.spherical_factory(:srid => 4326).point(l.longitude, l.latitude)
-
-    # Check to see if new dog status matches the last stored status. Don't save it if it does.
-    # This is a work around that solves a problem where editing a location would create a new
-    # dog status even if it didn't change. This is because we're not using the default active
-    # admin behaviour for nested form attributes, and not allowing dog statuses to be mutable.
-    # This is to maintain a history of dog statuses for a location, without users having to be
-    # aware of this. Although this is a little hacky, it's a large improvement from the usability
-    # standpoint. The default behaviour is to show
-    current_status = Location.find(l.id).dog_statuses.last
-    new_status = l.dog_statuses.last
-    if(current_status.guidelines == new_status.guidelines and
-       current_status.status == new_status.status)
-      new_status.destroy
-    end
   end
 
   index do
@@ -42,6 +28,7 @@ ActiveAdmin.register Location do
   # Form. Conditionally add geolocation values to lat/long fields. Required to manipulate geolocations standard
   # WKT output into seperately editable lat/long fields.
   form do |f|
+    f.semantic_errors *f.object.errors.keys
     f.inputs "Location Details" do
       f.input :name
       f.input :active, as: :select
@@ -58,13 +45,18 @@ ActiveAdmin.register Location do
         ? image_tag(f.object.image.url(:medium))
         : content_tag(:span, "no image uploaded yet")
     end
-
-    # Create a new dog status object by default. Gets around the default active admin behaviour of adding
-    # buttons to create a new status.
-    f.object.dog_statuses.new if f.object.dog_statuses.empty? 
-    f.inputs "Dog Information", for: [:dog_statuses, f.object.dog_statuses.last] do |ds|
-      ds.input :status, as: :select, collection: DogStatus.statuses.keys
-      ds.input :guidelines, as: :text
+    f.object.dog_statuses.new if f.object.new_record?
+    f.inputs do
+      f.has_many :dog_statuses, heading: 'Dog Details' do |ds|
+        if f.object.new_record? or ds.object.new_record?
+          ds.input :status, as: :select, collection: DogStatus.statuses.keys
+          ds.input :guidelines, as: :text
+        else
+          ds.input :created_at, as: :string, :input_html => { :readonly=>true, :disabled=>true }
+          ds.input :status, as: :select, collection: DogStatus.statuses.keys
+          ds.input :guidelines, as: :string
+        end
+      end
     end
     f.actions
   end
@@ -81,34 +73,16 @@ ActiveAdmin.register Location do
       row 'Current Dog Guidelines' do
         l.dog_statuses.last.guidelines
       end
-      row 'Previous Dog Statuses' do
-        li link_to "Click to view all previous", admin_location_dog_statuses_path(location_id: l.id)
-      end
       row :image do
         image_tag l.image.url(:medium)
       end
     end
   end
-
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # permit_params :list, :of, :attributes, :on, :model
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:permitted, :attributes]
-  #   permitted << :other if resource.something?
-  #   permitted
-  # end
-
-
 end
 
 # 
 ActiveAdmin.register DogStatus do
-  actions :all
+  actions :all, except: [:destroy, :new]
   belongs_to :location
-  config.filters = false
+  filter :location
 end
