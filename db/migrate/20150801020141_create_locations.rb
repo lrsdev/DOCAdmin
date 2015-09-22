2eccc70910a9e492ab5abe556f7de0b66e441c13
Samuel Stewart <samuelcstewart@icloud.com>
2015-09-22 23:56:42 +1200

    Remove default value from region field

--- db/migrate/20150801020141_create_locations.rb
+++ db/migrate/20150801020141_create_locations.rb
@@ -3,7 +3,7 @@ class CreateLocations < ActiveRecord::Migration
     create_table :locations do |t|
       t.timestamps null: false
       t.integer :category, null: false, default: 0
-      t.integer :region, null: false, default: 0
+      t.integer :region, null: false
       t.string :name, null: false
       t.text :animal_blurb, null: false
       t.st_point :geolocation, null: false, geographic: true
